3303afc6aa4f7d02c506a472ae9e2fa0e1599c0b
tiowill-dev <william_mw@outlook.com>
2026-06-19 15:44:33 -0300

    alteração do arquivo 14-doc-DOM.js

--- 14-doc-DOM.js
+++ 14-doc-DOM.js
@@ -4,8 +4,20 @@ const dollar = 5.14
 
 const convertValues = () => {
 const input = document.getElementById("valueInput").value
+const realValue = document.getElementById("real-value-txt")
+const dollarValue = document.getElementById("dolar-Value")
+//realValue.innerHTML = input
 
-console.log(input / dollar)
+realValue.innerHTML = new Intl.NumberFormat("pt-BR",{
+    style: "currency",
+    currency: "BRL"}).format(input)
+
+    dollarValue.innerHTML = new Intl.NumberFormat("en-US",{
+    style: "currency",
+    currency: "USD"}).format(input / dollar)
+
+
+//dollarValue.innerHTML = input / dollar
 }
 
 button.addEventListener('click',convertValues)
